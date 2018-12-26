30852b6b030a8e4513584e4046df452f2fdba0e6
Yao Ding <yao.ding@compass.com>
2018-12-26 15:22:36 -0500

    lint fix

--- src/components/Slideshow.vue
+++ src/components/Slideshow.vue
@@ -237,7 +237,6 @@ export default {
       Options.plugins.forEach(plugin => {
         plugin[0].init(this, plugin[1])
       })
-      
     },
     unregisterPlugins: function () {
       Options.plugins.forEach(plugin => {
